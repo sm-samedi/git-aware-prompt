4abd41f68f02652ed77531cd5bc294e8d460252e
Steven Muchmore <sm@srllc.com>
2016-07-06 12:35:06 -0400

    Add --branch flag

--- prompt.sh
+++ prompt.sh
@@ -12,7 +12,7 @@ find_git_branch() {
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
+  local status=$(git status --porcelain --branch 2> /dev/null)
   if [[ "$status" != "" ]]; then
     git_dirty='*'
   else
